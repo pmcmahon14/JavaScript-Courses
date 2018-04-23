5f761f0bd24b8bbf157236236868f6aeb76c1437
Patrick McMahon <pmcmahon14@gmail.com>
2018-04-23 11:50:14 -0700

    added event listeners to controller section and specified for return key

--- budget-app/app.js
+++ budget-app/app.js
@@ -18,6 +18,22 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    //some code
+    var ctrlAddItem = function() {
+        //TODO GET INPUT FIELD DATA
+        //TODO ADD ITEM TO BUDGET CONTROLLER
+        //TODO ADD NEW ITEM TO UI
+        //TODO CALCULATE BUDGET
+        //TODO DISPLAY BUDGET ON UI
+        console.log('it works');
+    }
 
-});
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
