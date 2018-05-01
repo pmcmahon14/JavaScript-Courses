c497c2d7335bb2766231b2263f6dd2ac0ef823f9
Patrick McMahon <pmcmahon14@gmail.com>
2018-04-30 21:18:59 -0700

    can add new item to budget controller

--- budget-app/app.js
+++ budget-app/app.js
@@ -1,8 +1,62 @@
 //BUDGET CONTROLLER
 var budgetController = (function(){
 
-    //some code
+    //VARIABLES FOR CONSTRUCTIOR FUNCTION TO ADD CURRENT ENTRY - EXPENSE OR INCOME
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    //STORE ENTRIES INTO ARRAY BY USING OBJECTS
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    //CONSTRUCTOR TO DETERMINE NEW ITEMS
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            //CREATE NEW ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //CREATE NEW ITEM BASED ON EXP OR INC TYPE
+            if (type === "exp") {
+                newItem = new Expense(ID, des, val);
+            } else if (type ==="inc") {
+                newItem = new Income(ID, des, val);
+            }
+
+            //PUSH INTO DATA STRUCTURE
+            data.allItems[type].push(newItem);
+
+            //RETURN NEW ELEMENT
+            return newItem;
+
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    }
 
 })();
 
@@ -18,7 +72,7 @@ var UIController = (function() {
     };
 
     return {
-       getinput: function() {
+       getInput: function() {
            return {
                type: document.querySelector(DOMstrings.inputType).value,  // WILL BE EITHER INCOME OR EXPENSE
                description: document.querySelector(DOMstrings.inputDescription).value,
@@ -40,10 +94,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMstrings();
+
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function(event) {
-
             if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
@@ -52,9 +106,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function() {
+        var input, newItem;
+
         //TODO GET INPUT FIELD DATA
-        var input = UICtrl.getinput();
+        input = UICtrl.getInput();
         //TODO ADD ITEM TO BUDGET CONTROLLER
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         //TODO ADD NEW ITEM TO UI
         //TODO CALCULATE BUDGET
         //TODO DISPLAY BUDGET ON UI
@@ -62,7 +120,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     return {
         init: function() {
-            console.log('started');
+            console.log('Application has started');
             setupEventListeners();
         }
     };
