2a01823aeed826741f9ecd3967a3ee66870b8e58
Patrick McMahon <pmcmahon14@gmail.com>
2018-05-12 14:00:32 -0700

    started updating percentages in budget controller

--- budget-app/app.js
+++ budget-app/app.js
@@ -6,6 +6,19 @@ var budgetController = (function(){
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value/totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     };
 
     var Income = function(id, description, value) {
@@ -101,6 +114,30 @@ var budgetController = (function(){
                 data.percentage = -1;
             }
 
+        },
+
+        calculatePercentage: function() {
+
+            /*
+            a=20
+            b=10
+            c=40
+            income=100
+            a=20/100=20%
+            b=10/100=10%
+            c=40/100=40%
+             */
+
+            data.allItems.exp.forEach(function(cur) {
+                cur.calcPercentage();
+            });
+        },
+
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function() {
@@ -251,10 +288,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updatePercentages = function() {
 
         //calculate percentages
+        budgetCtrl.calculatePercentages;
 
         //read percentages from budget controller
+        var percentages = budgetCtrl.getPercentages();
 
         //update UI with new percentages
+        console.log(percentages);
 
     };                                             //
 
